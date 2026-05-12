3eb6bde2cf1e6685c0c5edb2154836c8630d9766
Arturouu <arturocontrerastorres86@gmail.com>
2026-05-11 21:49:25 -0500

    chore: update app.vue file.

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,10 @@
 <script setup>
+import Layout from './shared/presentation/components/layout.vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 </script>
 
 <template>
+  <layout/>
 </template>
-
-<style scoped>
-</style>
-
