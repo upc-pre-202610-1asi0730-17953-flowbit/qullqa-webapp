c8e788b37dfe3e39125561e48df29fdccfa51e82
Arturouu <arturocontrerastorres86@gmail.com>
2026-06-11 21:43:36 -0500

    feat(iam): add authentication guard.

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import Home from './shared/presentation/views/home.vue';
 import productRoutes from './product/presentation/product.routes.js';
 import alertsRoutes from './alerts/presentation/alerts.routes.js';
 import salesRoutes from './sales/presentation/sales.routes.js';
+import { authenticationGuard } from './iam/infrastructure/authentication.guard.js';
 
 
 const pageNotFound = () => import('./shared/presentation/views/page-not-found.vue');
@@ -62,10 +63,9 @@ const router = createRouter({
  * @returns {void}
  */
 router.beforeEach((to, from, next) => {
-    console.log(`Navigating from ${from.name} to ${to.name}`);
-    const baseTitle    = 'Qullqa';
-    document.title     = `${baseTitle} - ${to.meta['title'] ?? ''}`;
-    return next();
+    const baseTitle = 'Qullqa';
+    document.title  = `${baseTitle} - ${to.meta['title'] ?? ''}`;
+    return authenticationGuard(to, from, next);
 });
 
 export default router;
